5b9a09e3cc298a3a76bdfeb227bf24766de405cf
Anne-4nthenya <annnenthenya@gmail.com>
2022-09-08 12:42:33 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,8 @@
 /**
  * main - Entry point
  *
- * Return:Always 0(success)
+ * Return: Always 0(success)
  */*
 int main(void)
 {
-	puts("\"Programming is building a multilingual puzzle");
-	return(0);
-}
+	
